79c0b495911828578d3b6c69fcc4c28ba105baa6
Jonathan <jonathan@elmgren.dev>
2023-01-11 11:06:00 +0100

    sendgrid integration for contact form

--- components/_contact/Contact.tsx
+++ components/_contact/Contact.tsx
@@ -1,25 +1,68 @@
-import React from 'react'
+'use client'
+import { ChangeEvent, useState } from 'react'
 import { Links } from '../_common/_links/Links'
 
+type message = {
+    status: 'error' | 'success',
+    msg: string
+}
+
 export const Contact = () => {
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' })
+    const [loading, setLoading] = useState(false)
+    const [msg, setMsg] = useState<message | undefined>(undefined)
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            const res = await fetch('/api/mail', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+
+            if (res.ok) {
+                setMsg({ status: 'success', msg: 'Thank you. I will be in contact with you shortly.' })
+            } else {
+                const json = await res.json()
+                if (json.error) {
+                    throw new Error(json.error)
+                }
+            }
+        } catch (e) {
+            if (e instanceof Error) {
+                setMsg({ status: 'error', msg: e.message })
+            } else {
+                setMsg({ status: 'error', msg: 'Something went wrong' })
+            }
+        }
+        setLoading(false)
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setFormData({ ...formData, [e.target.name]: e.target.value })
+
     return (
         <div className='max-w-2xl w-full'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 peer" placeholder=" " required />
-                        <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-primary duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                        <input value={formData.name} onChange={handleChange} type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 peer" placeholder=" " required />
+                        <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-primary duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                     </div>
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 peer" placeholder=" " required />
-                        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-primary duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                        <input value={formData.email} onChange={handleChange} type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 peer" placeholder=" " required />
+                        <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-primary duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                     </div>
                 </div>
                 <div>
-                    <label htmlFor="floating_message" className="block py-2.5 px-0 w-full text-sm text-primary">Message</label>
-                    <textarea className='border-0 bg-transparent border-b-2 border-primary resize-none w-full focus:h-52 h-6 transition-[height] overflow-hidden p-2 focus:border-2 focus:outline-none' name="floating_message" id="floating_message" placeholder="" />
+                    <label htmlFor="message" className="block py-2.5 px-0 w-full text-sm text-primary">Message</label>
+                    <textarea value={formData.message} name="message" required onChange={handleChange} className={`border-0 bg-transparent border-b-2 border-primary resize-none w-full ${formData.message.length > 1 ? 'h-52' : 'h-12'} transition-[height] overflow-hidden p-2 focus:border-2 focus:outline-none`} id="floating_message" placeholder="" />
                 </div>
-                <button type="submit" className="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center ml-auto block mt-5">Submit</button>
+                <button disabled={loading} type="submit" className="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center ml-auto block mt-5">{loading ? 'Loading...' : 'Submit'}</button>
+                {msg && <p className={`${msg.status === 'success' ? 'text-green-300' : 'text-red-300'} p-4 capitalize text-xl text-center`}>{msg.status}: {msg.msg}</p>}
             </form>
             <div className='mt-6 max-h-7'>
                 <Links center />
