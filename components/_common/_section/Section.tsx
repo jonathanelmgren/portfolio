a095bebd1e1a4391c222ce498fbd466f51d5575c
Jonathan <jonathan@elmgren.dev>
2023-01-13 12:14:49 +0100

    add analytics

--- components/_common/_section/Section.tsx
+++ components/_common/_section/Section.tsx
@@ -10,11 +10,11 @@ interface Props {
 export const Section = ({ title, children, anchor }: Props) => {
 
   return (
-    <div id={anchor.replace('#', '')} className='min-h-screen text-justify flex gap-6 flex-col items-center justify-center mx-[5%] md:mx-[10%]'>
+    <section id={anchor.replace('#', '')} className='min-h-screen text-justify flex gap-6 flex-col items-center justify-center mx-[5%] md:mx-[10%]'>
       {title && <h3 className='clamp-text text-primary opacity-10 leading-[0.8] -z-10 max-w-7xl mt-4'>{title}</h3>}
       <div className='w-full my-auto pb-12'>
         {children}
       </div>
-    </div>
+    </section>
   )
 }
--- components/_contact/Contact.tsx
+++ components/_contact/Contact.tsx
@@ -59,7 +59,7 @@ export const Contact = () => {
                 </div>
                 <div>
                     <label htmlFor="message" className="block py-2.5 px-0 w-full text-sm text-primary">Message</label>
-                    <textarea value={formData.message} name="message" required onChange={handleChange} className={`border-0 bg-transparent border-b-2 border-primary resize-none w-full ${formData.message.length > 1 ? 'h-52' : 'h-12'} transition-[height] overflow-hidden p-2 focus:border-2 focus:outline-none`} id="floating_message" placeholder="" />
+                    <textarea id="message" value={formData.message} name="message" required onChange={handleChange} className={`border-0 bg-transparent border-b-2 border-primary resize-none w-full ${formData.message.length > 1 ? 'h-52' : 'h-12'} transition-[height] overflow-hidden p-2 focus:border-2 focus:outline-none`} placeholder="" />
                 </div>
                 <div className='flex justify-between mt-5'>
                     <button disabled={loading} type="submit" className="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-12 py-2.5 text-center block">{loading ? 'Loading...' : 'Submit'}</button>
